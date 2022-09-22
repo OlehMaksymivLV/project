df78bd2ca7ce9e90caf6a5c42d95099132f0446e
Oleg <oleglvivmaks@gmail.com>
2022-09-22 23:28:04 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,86 @@
+"use strict";
+
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+// console.log(number);
+// number = 10;
+
+
+// const obj = {
+//     a: 50  
+// };
+
+// obj.a = 100;
+
+// console.log(obj);
+
+
+// console.log(name);
+// var name = 'Oleh';
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
+
+// const obj = {
+//     name: "Ivan",
+//     age: 27,
+//     isMarrried: false
+// }
+
+// // console.log(obj.name);
+// console.log(obj["name"]);
+
+// const salaries = {
+//     'Oleh': 3300,
+//     'Lyana': 700
+// };
+
+// // console.log(salaries.Lyana);
+// let b = 222;
+// salaries[b] = 123;
+
+// console.log(salaries[b]);
+
+// alert("Hello");
+
+// const result = confirm("Are sure you want to close this page?");
+// const answer = +prompt("Are you 18 years old?", "18");
+
+// console.log(answer);
+
+// const answers = [];
+
+// // answers[0] = prompt('Як ваші справи?', '');
+// // answers[1] = prompt('Як ваш робота?', '');
+// // answers[2] = prompt('Як ваші діти?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`)
+
+// const userName = 'Oleh';
+
+// alert(`Hello, ${userName}`);
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// const isChecked = true,
+//     isClose = true;
+
+// console.log(isChecked || !isClose);    
+
